b5fe1f9f4b9ae27e9331ecb0567c94dcc2effe59
mobiltek26 <mobiltek26@mobiltek26s-iMac.local>
2016-09-06 15:44:08 +0300

    Do Notification Tabs

---
+++ app/src/main/java/com/asktroapp/myapplication/NotificationTabsPagerAdapter.java
@@ -0,0 +1,7 @@
+package com.asktroapp.myapplication;
+
+/**
+ * Created by mobiltek26 on 9/6/16.
+ */
+public class NotificationTabsPagerAdapter {
+}
---
+++ app/src/main/java/com/asktroapp/myapplication/PurposesInFragment.java
@@ -0,0 +1,13 @@
+package com.asktroapp.myapplication;
+
+import android.app.Fragment;
+
+/**
+ * Created by mobiltek26 on 9/6/16.
+ */
+public class PurposesInFragment extends Fragment {
+
+
+
+
+}
